efc0f2c1afce554bf5390a92dfabeb8ce0382ed9
Hamid.hrs <hamid47.hrs@gmail.com>
2022-05-31 20:10:13 +0430

    Remove static audio files import, Create a map function to add Audio component based on dynamic audio files.

--- audio-survey-form/src/Components/AudioSecion.jsx
+++ audio-survey-form/src/Components/AudioSecion.jsx
@@ -1,8 +1,4 @@
 import Audio from "./Audio";
-import j1 from "../Audios/Jazz/bossa-nova-piano-instrumental-brasil-5732.mp3";
-import j2 from "../Audios/Jazz/desitalk-exclusive-life-is-strange-cold-breeze-acoustic-version-15168.mp3";
-import j3 from "../Audios/Jazz/nightlife-michael-kobrin-95bpm-3783.mp3";
-import j4 from "../Audios/Jazz/spanish-beach-109195.mp3";
 
 /*___________________________________________________________________________________*/
 
@@ -12,24 +8,17 @@ const AudioSecion = (props) => {
       className="mt-5 p-4 d-flex flex-column gap-4 border rounded-3"
       style={{ width: "360px" }}
     >
-      <h3 className="text-center">
-        {props.type}
-      </h3>
+      <h3 className="text-center">{props.type}</h3>
       <ul>
-        <Audio name="Bossa Nova" id={j1} rates={props.rates} setRate={props.setRates} />
-        <Audio
-          name="Desitalk Exclusive Life"
-          id={j2}
-          rates={props.rates}
-          setRate={props.setRates}
-        />
-        <Audio
-          name="Nightlife Michael Kobrin"
-          id={j3}
-          rates={props.rates}
-          setRate={props.setRates}
-        />
-        <Audio name="Spanish beach" id={j4} rates={props.rates} setRate={props.setRates} />
+        {props.audio.map((item, index) => (
+          <Audio
+            key={index}
+            name={props.rates[index].name}
+            source={item}
+            rates={props.rates}
+            setRate={props.setRates}
+          />
+        ))}
         <textarea
           rows={4}
           className="w-100 p-2"
